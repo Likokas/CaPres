8c531f22140e893aec60f6a9ac1949ef7c5f3f87
vinchrist21 <62634885+vinchrist21@users.noreply.github.com>
2020-12-13 21:53:02 +0700

    perpindahan dari splash ke login

--- app/src/main/java/com/example/capres/ui/splash/SplashActivity.java
+++ app/src/main/java/com/example/capres/ui/splash/SplashActivity.java
@@ -2,15 +2,38 @@ package com.example.capres.ui.splash;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.Window;
+import android.view.WindowManager;
 
 import com.example.capres.R;
+import com.example.capres.ui.login.LoginActivity;
 
 public class SplashActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getSupportActionBar().hide();
+
         setContentView(R.layout.activity_splash);
+
+        Thread thread = new Thread(){
+            public void run(){
+                try {
+                    sleep(4000);
+                } catch (InterruptedException e){
+                    e.printStackTrace();
+                } finally {
+                    startActivity(new Intent(SplashActivity.this, LoginActivity.class));
+                    finish();
+                }
+            }
+        };
+        thread.start();
     }
 }
