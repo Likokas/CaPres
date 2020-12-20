1051c71aa2b42b72f8cdefa896bb6bbecc2f9d40
Likokas <62798918+Likokas@users.noreply.github.com>
2020-12-20 20:30:57 +0800

    login logout

--- app/src/main/java/com/example/capres/ui/admin/AdminActivity.java
+++ app/src/main/java/com/example/capres/ui/admin/AdminActivity.java
@@ -6,6 +6,8 @@ import androidx.lifecycle.ViewModelProviders;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.Toast;
 
@@ -24,6 +26,9 @@ public class AdminActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getSupportActionBar().hide();
         setContentView(R.layout.activity_admin);
 
         viewModel = ViewModelProviders.of(this).get(AdminViewModel.class);
